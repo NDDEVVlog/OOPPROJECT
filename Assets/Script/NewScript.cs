960d59898ea9aecca12010ec45949606d2f9340e
S0meDudeIdk <phamvuhoangbao2004@outlook.com>
2023-09-24 09:28:19 +0700

    I just added a comment

---
+++ Assets/Script/NewScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // This is a line
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
