1b96ebb8d84c518f486085188e882e75a8b17af3
NDDEVVlog <nddevgame@gmail.com>
2023-10-02 19:28:21 +0700

    adjust PlayerOneWayPlatform to OneWayPlatform

--- Assets/Script/Function/OnewayPlatform.cs
+++ Assets/Script/Function/OnewayPlatform.cs
@@ -1,7 +1,8 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerOneWayPlatform : MonoBehaviour
+public class OneWayPlatform : MonoBehaviour
 {
     private GameObject currentOneWayPlatform;
 
