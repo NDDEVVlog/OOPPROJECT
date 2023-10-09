46f4b47414cb007d6e9adf8d661e0fbac3f7c018
vlmd091104 <vuleminhduc19@gmail.com>
2023-10-09 23:47:01 +0700

    jump animation
    còn nhiều lỗi vcb nên đừng ai đụng

--- Assets/jump2.cs
+++ Assets/jump2.cs
@@ -5,19 +5,30 @@ using UnityEngine;
 public class jump2 : MonoBehaviour
 {
     private Rigidbody2D playerRb;
+    private CollisionDataRecieve collisiondataretrieve;
     // Start is called before the first frame update
+    public Animator animator;
     void Start()
     {
         playerRb = GetComponent<Rigidbody2D>();
-        
+    }
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        collisiondataretrieve = GetComponent<CollisionDataRecieve>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space)) 
-        {
+        {   
+            animator.SetBool("isJumping", true);
             playerRb.AddForce(Vector2.up * 250);
+        }
+        if (collisiondataretrieve.OnGround == true)
+        {
+            animator.SetBool("isJumping", false);
         }
     }
 }
